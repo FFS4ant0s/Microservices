63ac21056f93c8a7fd7090a064a4aea6004916a1
Fernando <silentguyband@gmail.com>
2024-12-30 16:10:41 -0300

    UserRecordDto

---
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.ms.user.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.user.dtos.UserRecordDto;
+import com.ms.user.models.UserModel;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@ResquestBody @Valid UserRecordDto userRecordDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+}
